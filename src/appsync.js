e025ac689669f3cd3a0af6258f53d50b4e895393
nguyen tien nghiep <nguyen_tien_nghiep@mediado.local>
2018-07-12 12:39:11 +0900

    fix awsappsycn

--- src/appsync.js
+++ src/appsync.js
@@ -1,6 +1,6 @@
 ﻿export default {
-    "graphqlEndpoint": "https://sclgmjfztneezka6jcqapl2yci.appsync-api.us-east-2.amazonaws.com/graphql",
-    "region": "us-east-2",
+    "graphqlEndpoint": "https://enht2f5abrcqdjxyh4jlnixmhu.appsync-api.us-west-2.amazonaws.com/graphql",
+    "region": "us-west-2",
     "authenticationType": "API_KEY",
-    "apiKey": "da2-aecuqn5iwrhnzfvetosq7dlw7q"
+    "apiKey": "da2-5sk2flcj7jei5cxje4pkxssrda"
 }
